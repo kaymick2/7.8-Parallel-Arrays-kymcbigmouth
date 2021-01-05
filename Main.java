1f75755934ea8f90fd8b90cced2dbc03f3ea177e
Kyle McDowell <kymcd21@icstudents.org>
2021-01-05 05:22:18 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+class Main {
+  public static void main(String[] args) {
+  String[] name = new String[5];
+  double[] price = new double[5];
+  int[] quantity = new int[5];
+  double[] revenue = new double[5];
+  
+
+    }
+  }
